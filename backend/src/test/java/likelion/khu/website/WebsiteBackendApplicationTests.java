6d23f4f63e84333235fe11f09fd64ab0f5273fca
sunwoo <117715215+sunwoo1256@users.noreply.github.com>
2026-06-27 22:01:42 +0900

    feat: backend springboot 초기 세팅 (#2) (#10)

---
+++ backend/src/test/java/likelion/khu/website/WebsiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion.khu.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
